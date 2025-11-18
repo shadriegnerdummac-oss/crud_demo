3799967555846963283a15c834831e95969b71dc
SRDummac <shadriegnerdummac@gmail.com>
2025-11-18 09:50:27 +0800

    Connected to MySQL Database

--- crud_demo/Form1.Designer.vb
+++ crud_demo/Form1.Designer.vb
@@ -22,10 +22,108 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.TextBoxAge = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxEmail = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ButtonInsert = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonConnect
+        '
+        Me.ButtonConnect.Location = New System.Drawing.Point(251, 81)
+        Me.ButtonConnect.Name = "ButtonConnect"
+        Me.ButtonConnect.Size = New System.Drawing.Size(147, 80)
+        Me.ButtonConnect.TabIndex = 0
+        Me.ButtonConnect.Text = "Connect MySQL"
+        Me.ButtonConnect.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(204, 174)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "NAME:"
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Location = New System.Drawing.Point(251, 167)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(377, 20)
+        Me.TextBoxName.TabIndex = 2
+        '
+        'TextBoxAge
+        '
+        Me.TextBoxAge.Location = New System.Drawing.Point(251, 193)
+        Me.TextBoxAge.Name = "TextBoxAge"
+        Me.TextBoxAge.Size = New System.Drawing.Size(377, 20)
+        Me.TextBoxAge.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(204, 200)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "AGE:"
+        '
+        'TextBoxEmail
+        '
+        Me.TextBoxEmail.Location = New System.Drawing.Point(251, 219)
+        Me.TextBoxEmail.Name = "TextBoxEmail"
+        Me.TextBoxEmail.Size = New System.Drawing.Size(377, 20)
+        Me.TextBoxEmail.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(204, 226)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "EMAIL:"
+        '
+        'ButtonInsert
+        '
+        Me.ButtonInsert.Location = New System.Drawing.Point(251, 245)
+        Me.ButtonInsert.Name = "ButtonInsert"
+        Me.ButtonInsert.Size = New System.Drawing.Size(147, 80)
+        Me.ButtonInsert.TabIndex = 7
+        Me.ButtonInsert.Text = "CREATE (Insert Dara)"
+        Me.ButtonInsert.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonInsert)
+        Me.Controls.Add(Me.TextBoxEmail)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxAge)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxName)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ButtonConnect)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ButtonConnect As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents TextBoxAge As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxEmail As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ButtonInsert As Button
 End Class
--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -1,3 +1,25 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
 
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
+
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=localhost; userid=root; password=root; database=crud_demo_db"
+
+        Try
+            conn.Open()
+            MessageBox.Show("Connected")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            conn.Close()
+        End Try
+
+
+    End Sub
+
+    Private Sub ButtonInsert_Click(sender As Object, e As EventArgs) Handles ButtonInsert.Click
+
+    End Sub
 End Class
