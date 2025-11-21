c24f8c531c8e3ee12c65a1266119f874a0282bd9
SRDummac <shadriegnerdummac@gmail.com>
2025-11-21 08:12:45 +0800

    with (Read retrieve dat)
    using adaptor to display to datagridview

--- crud_demo/Form1.Designer.vb
+++ crud_demo/Form1.Designer.vb
@@ -30,11 +30,14 @@ Partial Class Form1
         Me.TextBoxEmail = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.ButtonInsert = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ButtonRead = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ButtonConnect
         '
-        Me.ButtonConnect.Location = New System.Drawing.Point(251, 81)
+        Me.ButtonConnect.Location = New System.Drawing.Point(12, 12)
         Me.ButtonConnect.Name = "ButtonConnect"
         Me.ButtonConnect.Size = New System.Drawing.Size(147, 80)
         Me.ButtonConnect.TabIndex = 0
@@ -44,7 +47,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(204, 174)
+        Me.Label1.Location = New System.Drawing.Point(177, 19)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(41, 13)
         Me.Label1.TabIndex = 1
@@ -52,14 +55,14 @@ Partial Class Form1
         '
         'TextBoxName
         '
-        Me.TextBoxName.Location = New System.Drawing.Point(251, 167)
+        Me.TextBoxName.Location = New System.Drawing.Point(224, 12)
         Me.TextBoxName.Name = "TextBoxName"
         Me.TextBoxName.Size = New System.Drawing.Size(377, 20)
         Me.TextBoxName.TabIndex = 2
         '
         'TextBoxAge
         '
-        Me.TextBoxAge.Location = New System.Drawing.Point(251, 193)
+        Me.TextBoxAge.Location = New System.Drawing.Point(224, 38)
         Me.TextBoxAge.Name = "TextBoxAge"
         Me.TextBoxAge.Size = New System.Drawing.Size(377, 20)
         Me.TextBoxAge.TabIndex = 4
@@ -67,7 +70,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(204, 200)
+        Me.Label2.Location = New System.Drawing.Point(177, 45)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(32, 13)
         Me.Label2.TabIndex = 3
@@ -75,7 +78,7 @@ Partial Class Form1
         '
         'TextBoxEmail
         '
-        Me.TextBoxEmail.Location = New System.Drawing.Point(251, 219)
+        Me.TextBoxEmail.Location = New System.Drawing.Point(224, 64)
         Me.TextBoxEmail.Name = "TextBoxEmail"
         Me.TextBoxEmail.Size = New System.Drawing.Size(377, 20)
         Me.TextBoxEmail.TabIndex = 6
@@ -83,7 +86,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(204, 226)
+        Me.Label3.Location = New System.Drawing.Point(176, 67)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(42, 13)
         Me.Label3.TabIndex = 5
@@ -91,18 +94,37 @@ Partial Class Form1
         '
         'ButtonInsert
         '
-        Me.ButtonInsert.Location = New System.Drawing.Point(251, 245)
+        Me.ButtonInsert.Location = New System.Drawing.Point(607, 7)
         Me.ButtonInsert.Name = "ButtonInsert"
         Me.ButtonInsert.Size = New System.Drawing.Size(147, 80)
         Me.ButtonInsert.TabIndex = 7
         Me.ButtonInsert.Text = "CREATE (Insert Dara)"
         Me.ButtonInsert.UseVisualStyleBackColor = True
         '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(305, 179)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(449, 259)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'ButtonRead
+        '
+        Me.ButtonRead.Location = New System.Drawing.Point(482, 93)
+        Me.ButtonRead.Name = "ButtonRead"
+        Me.ButtonRead.Size = New System.Drawing.Size(147, 80)
+        Me.ButtonRead.TabIndex = 9
+        Me.ButtonRead.Text = "READ (Retrieve Dara)"
+        Me.ButtonRead.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonRead)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.ButtonInsert)
         Me.Controls.Add(Me.TextBoxEmail)
         Me.Controls.Add(Me.Label3)
@@ -113,6 +135,7 @@ Partial Class Form1
         Me.Controls.Add(Me.ButtonConnect)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -126,4 +149,6 @@ Partial Class Form1
     Friend WithEvents TextBoxEmail As TextBox
     Friend WithEvents Label3 As Label
     Friend WithEvents ButtonInsert As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ButtonRead As Button
 End Class
--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -20,7 +20,7 @@ Public Class Form1
     End Sub
 
     Private Sub ButtonInsert_Click(sender As Object, e As EventArgs) Handles ButtonInsert.Click
-        Dim query As String = "INSERT INTO students_tbl (name, age, email) VALUES (name, age, email)"
+        Dim query As String = "INSERT INTO students_tbl (name, age, email) VALUES (@name, @age, @email)"
         Try
             Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
                 conn.Open()
@@ -31,6 +31,20 @@ Public Class Form1
                     cmd.ExecuteNonQuery()
                     MessageBox.Show("Record Insert Successfully!")
                 End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
+        Dim query As String = "SELECT * FROM crud_demo_db.students_tbl;"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()    'Table Object
+                adapter.Fill(table)     'From Adapter to Table Object
+                DataGridView1.DataSource = table    'Display to DataGridView
             End Using
         Catch ex As Exception
             MsgBox(ex.Message)
